b22e7ca67f145adbc25872bebf1d32c5eb646160
Aaron Kromer <kromer.aaron@gmail.com>
2025-11-14 11:09:58 +0100

    add base gestures, pinch, grab and resize

--- uebungen/20_HandPoseBoilerplate/sketch.js
+++ uebungen/20_HandPoseBoilerplate/sketch.js
@@ -21,6 +21,12 @@ let video;              // Die Webcam
 let hands = [];         // Array mit allen erkannten Händen
 let ratio;              // Skalierungsfaktor zwischen Video und Canvas
 let isModelReady = false; // Flag, ob das Modell geladen und Hände erkannt wurden
+let touchPoints = []; // Array für benutzerdefinierte Touch-Punkte
+let touchPointDiameter = 60; // Durchmesser der Touch-Punkte
+const HAND_COLLISION_POINT = "index_finger_tip"; // Index des Zeigefinger-Tipps im Keypoint-Array
+const PINCH_POINTS = ["thumb_tip", "index_finger_tip"]; // Keypoints für Pinch-Geste
+const keypointDiameter = 20; // Durchmesser der Keypoint-Kreise
+let shouldPointsBeInvisible = false;
 
 /**
  * Lädt das HandPose-Modell vor dem Setup
@@ -62,13 +68,57 @@ function draw() {
 
   //Zeige das Video (optional)
   image(video, 0, 0, video.width * ratio, video.height * ratio);
-  
+
   // Zeichne nur, wenn das Modell bereit ist und Hände erkannt wurden
   if (isModelReady) {
     drawHandPoints();
     
-    // HIER KÖNNEN EIGENE/Andere ZEICHNUNGEN Oder Interaktionen HINZUGEFÜGT WERDEN
+    if (!shouldPointsBeInvisible) {
+      drawTouchPoints();
+    }
     
+    // removeTouchPointsOnCollision();
+
+    if (isGrabbingTouchPoint()) {
+      // update touchpoint pos based on pinch pos
+      const hand = hands[0];
+      const [p1, p2] = PINCH_POINTS.map(p => hand[p]);
+      const pinchX = (p1.x + p2.x) / 2 * ratio;
+      const pinchY = (p1.y + p2.y) / 2 * ratio;
+
+      touchPoints[0].x = pinchX;
+      touchPoints[0].y = pinchY;
+    }
+
+    if (isHoldingTouchPoint()) {
+      // remove touch point
+      shouldPointsBeInvisible = true;
+
+      const hand = hands[0];
+        
+      if (hand == null) return;
+      const wrist = hand["wrist"];
+      touchPoints = [];
+      touchPoints.push(
+        createVector(
+          wrist.x * ratio,
+          wrist.y * ratio - 80
+        )
+      );
+    } else {
+      shouldPointsBeInvisible = false;
+    }
+
+    detectResize();
+  } else {
+    if (touchPoints.length === 0) {
+      touchPoints.push(
+        createVector(
+          random(10 + touchPointDiameter, windowWidth - 10 - touchPointDiameter),
+          random(10 + touchPointDiameter, windowHeight - 10 - touchPointDiameter),
+        )
+      );
+    }
   }
   
   pop();
@@ -105,8 +155,101 @@ function drawHandPoints() {
       // Zeichne Keypoint als grüner Kreis
       fill(0, 255, 0);
       noStroke();
-      circle(keypoint.x * ratio, keypoint.y * ratio, 10);
+      circle(keypoint.x * ratio, keypoint.y * ratio, keypointDiameter);
     }
   }
 }
 
+function drawTouchPoints() {
+  fill(255, 0, 0);
+  noStroke();
+  touchPoints.forEach(tp => {
+    circle(tp.x, tp.y, touchPointDiameter);
+  });
+}
+
+function removeTouchPointsOnCollision() {
+  const newTouchPoints = touchPoints.filter(tp => {
+    return !hands.some(hand => {
+      const keypoint = hand[HAND_COLLISION_POINT];
+      const d = dist(tp.x, tp.y, keypoint.x * ratio, keypoint.y * ratio);
+      if (d < (touchPointDiameter / 2 + keypointDiameter / 2)) {
+        return true; 
+      }
+
+      return false;
+    });
+  });
+  touchPoints = newTouchPoints;
+}
+
+function isGrabbingTouchPoint() {
+  if (hands.length !== 1) return false;
+
+  return touchPoints.some(tp => {
+    return hands.some(hand => {
+      const [p1, p2] = PINCH_POINTS.map(p => hand[p]);
+
+      const pinchDistance = dist(p1.x * ratio, p1.y * ratio, p2.x * ratio, p2.y * ratio);
+      const isPinching = pinchDistance < 60;
+      if (isPinching) {
+        const distanceToTouchPoint = dist(tp.x, tp.y, (p1.x + p2.x) / 2 * ratio, (p1.y + p2.y) / 2 * ratio);
+        if (distanceToTouchPoint < (touchPointDiameter / 2 + keypointDiameter / 2)) {
+          return true;
+        }
+      }
+    });
+  });
+}
+
+function isHoldingTouchPoint() {
+  if (hands.length !== 1) return false;
+  
+  return touchPoints.some(tp => {
+    return hands.some(hand => {
+      const averageDistanceToWrist = Object.keys(hand)
+        .filter(k =>  k.endsWith("_tip"))
+        .filter(k =>  k !== "thumb_tip")
+        .map(k => hand[k])
+        .map(tip => {
+          const wrist = hand["wrist"];
+          return dist(wrist.x * ratio, wrist.y * ratio, tip.x * ratio, tip.y * ratio);
+        })
+        .reduce((sum, d) => sum + d, 0) / 4;
+
+      if (averageDistanceToWrist < 250) {
+        // is making a fist
+
+        if (shouldPointsBeInvisible) return true;
+
+        // get bounds of hands
+        const bounds = hand.keypoints.reduce((bounds, keypoint) => {
+          const x = keypoint.x * ratio;
+          const y = keypoint.y * ratio;
+          if (x < bounds.minX) bounds.minX = x;
+          if (x > bounds.maxX) bounds.maxX = x;
+          if (y < bounds.minY) bounds.minY = y;
+          if (y > bounds.maxY) bounds.maxY = y;
+          return bounds;
+        }, { minX: Infinity, maxX: -Infinity, minY: Infinity, maxY: -Infinity });
+
+        if (tp.x > bounds.minX && tp.x < bounds.maxX && tp.y > bounds.minY && tp.y < bounds.maxY) {
+          return true;
+        }
+      }
+    });
+  });
+}
+
+function detectResize() {
+  if (hands.length !== 2) return;
+
+  const fingerTips = hands.map(hand => hand["index_finger_tip"]);
+  const firstFingerDistanceToTouchPoint = dist(fingerTips[0].x * ratio, fingerTips[0].y * ratio, touchPoints[0].x, touchPoints[0].y);
+  const secondFingerDistanceToTouchPoint = dist(fingerTips[1].x * ratio, fingerTips[1].y * ratio, touchPoints[0].x, touchPoints[0].y);
+
+  if (firstFingerDistanceToTouchPoint > touchPointDiameter / 2 || secondFingerDistanceToTouchPoint > touchPointDiameter / 2) return;
+
+  const distanceBetweenFingers = dist(fingerTips[0].x * ratio, fingerTips[0].y * ratio, fingerTips[1].x * ratio, fingerTips[1].y * ratio);
+  touchPointDiameter = distanceBetweenFingers + 20;
+}
